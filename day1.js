346b60e4196e9e0166fcd87ceec2e2653e3ac479
MiwakoMotoya <boochiyaay@gmail.com>
2026-02-25 22:14:08 +0900

    Day1: basic variable and calculation practice

---
+++ day1.js
@@ -0,0 +1,16 @@
+let name = "Miwako";
+console.log(name);
+
+let stock = 10;
+console.log(stock);
+
+let a = 5;
+let b = 3;
+console.log(a + b);
+
+let price = 1200;
+let quantity = 3;
+
+let total = price * quantity;
+
+console.log("合計金額は " + total + " 円です");
