5f12961f56e2d32b1561121a49d5b31b86d80933
MiwakoMotoya <boochiyaay@gmail.com>
2026-03-06 20:21:55 +0900

    Add Day11 functions and update study log

---
+++ day11.js
@@ -0,0 +1,13 @@
+console.log("Day11スタート！");
+
+// 関数を2つ作ってみる
+function double(num) {
+  return num * 2;
+}
+
+function triple(num) {
+  return num * 3;
+}
+
+console.log(double(5));  // 10
+console.log(triple(5));  // 15
